fd7bdbefe3a88cb029553924c5ff4a8755de601c
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2016-07-12 01:53:28 +0200

    Convert script to sh

--- zenv.plugin.sh
+++ zenv.plugin.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 # TODO: If ZENV_ROOT!=zenv_pwd, source new .zenv and reset ZENV_ROOT
 # TODO: Add verification for new zenv and updated zenv
 
-function chpwd {
-    local zenv_pwd="$(zenv_locate_root)"
+chpwd() {
+    zenv_pwd="$(zenv_locate_root)"
     zenv_eval_interface
-    local interface=$?
+    interface=$?
 
     if [ "$zenv_pwd" = "" ] && [ "$ZENV_ROOT" != "" ] ; then
         echo "exit env"
@@ -34,11 +35,11 @@ function chpwd {
     fi
 }
 
-function zenv_eval_interface {
+zenv_eval_interface() {
     which zenv_up 1>/dev/null 2>&1
-    local up=$?
+    up=$?
     which zenv_down 1>/dev/null 2>&1
-    local down=$?
+    down=$?
 
     if [ $up -eq 0 ] && [ $down -eq 0 ] ; then
         return 0
@@ -47,12 +48,11 @@ function zenv_eval_interface {
     return 1
 }
 
-function zenv_locate_root {
-    local zenv_root
+zenv_locate_root() {
     zenv_root=$(pwd -P 2>/dev/null || command pwd)
     while [ ! -e "$zenv_root/.zenv" ] ; do
         zenv_root=${zenv_root%/*}
         if [ "$zenv_root" = "" ] ; then break; fi
     done
-    echo $zenv_root
+    echo "$zenv_root"
 }
